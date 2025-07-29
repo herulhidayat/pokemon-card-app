1c8d5f63364c0af62a878ac49d6daf897ae36cf2
Khairul Hidayat <khairul.hidayat@jkt1.ebdesk.com>
2025-07-29 17:55:15 +0700

    [BUG] check for ssr issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Container } from "@mui/material";
 import HeroFeature from "@/components/Features/Home/HeroFeature";
 import PokeCardListFeature from "@/components/Features/Home/PokeCardListFeature";
